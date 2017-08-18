3ecf5fce23c597e6d1280d36394d47f7b87cf14e
Hayden <bbconlinenews@yahoo.com>
2017-08-18 17:04:29 +0800

    Invoke once when created.
    Class will invoke the first loop when it created.

--- TempFolder/liveSession.js
+++ TempFolder/liveSession.js
@@ -1,4 +1,5 @@
-//just an idea on using sessionStorage/localStorage.
+
+//just an idea on using sessionStorage.
 /*******************************************
  * 
  * @param   string      itemName        Item's name for sessionStorage
@@ -13,6 +14,19 @@
 function LiveSession(itemName, resquestCall, willUpdateCall, expiry){
 
     this.source = null;
+
+    //contructer of this class, where it will invoke the first loop without condition.
+    var currTimestamp = Math.floor(Date.now()/1000);
+    var successCall = resquestCall();
+    successCall(
+        function(response){
+            var stringResponse = JSON.stringify(response);
+            willUpdateCall(response);
+            sessionStorage.setItem(itemName, stringResponse);
+            sessionStorage.setItem(itemName+'Expiry', currTimestamp+expiry);
+            this.source = stringResponse;
+        }.bind(this)
+    )
 
     this.run = function(){
         var currTimestamp = Math.floor(Date.now()/1000);
@@ -40,15 +54,32 @@ function LiveLocal(itemName, resquestCall, willUpdateCall, expiry){
 
     this.source = null;
 
+    //contructer of this class, where it will invoke the first loop without condition.
+    var currTimestamp = Math.floor(Date.now()/1000);
+    var successCall = resquestCall();
+    successCall(
+        function(response){
+            var stringResponse = JSON.stringify(response);
+            willUpdateCall(response);
+            localStorage.setItem(itemName, stringResponse);
+            localStorage.setItem(itemName+'Expiry', currTimestamp+expiry);
+            this.source = stringResponse;
+        }.bind(this)
+    )
+
+
     this.run = function(){
+        console.log('invoked');
         var currTimestamp = Math.floor(Date.now()/1000);
         if(this.source == null || (currTimestamp > localStorage.getItem(itemName+'Expiry'))){
 
-            successCall = resquestCall();
+            var successCall = resquestCall();
             successCall(
                 function(response){
+                    console.log(successCall);
                     var stringResponse = JSON.stringify(response);
                     if(stringResponse !== localStorage.getItem(itemName)){
+                        console.log('will update!');
                         willUpdateCall(response);
                     }
                     localStorage.setItem(itemName, stringResponse);
@@ -57,7 +88,11 @@ function LiveLocal(itemName, resquestCall, willUpdateCall, expiry){
                 }.bind(this)
             )
         }else if(this.source !== localStorage.getItem(itemName)){
+            console.log('no fetch but update')
             this.source = localStorage.getItem(itemName);
             willUpdateCall(JSON.parse(this.source));
         }
+
     }.bind(this);
+}
+
