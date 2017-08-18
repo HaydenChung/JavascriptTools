f0587f645681de3c35f5b77c8ad34cd58eb32b70
Hayden <bbconlinenews@yahoo.com>
2017-08-18 12:59:11 +0800

    Update liveSession.js

--- TempFolder/liveSession.js
+++ TempFolder/liveSession.js
@@ -1,33 +1,63 @@
-//just an idea on using sessionStorage.
+//just an idea on using sessionStorage/localStorage.
+/*******************************************
+ * 
+ * @param   string      itemName        Item's name for sessionStorage
+ * @param   function    resquestCall    Callback to fetch source,should return another function/promise.
+ * @param   function    willUpdateCall  Callback to invoke when source is updated. 
+ * @param   integer     expiry          Seconds before the item is expire, where it will invoke the resquestCall.In other word, when will the data refresh.
+ * @return  Null.
+ * @method  function    run             Pass this method as a parameter of setTimeout or setInterval function.
+ * 
+ *******************************************/
 
-function getSession(itemName,resquestCall, willUpdateCall,interval){
-    var source = sessionStorage.getItem(itemName);
-    var currTimestamp = Math.floor(Date.now()/1000);
-    if(source == null || (currTimestamp> sessionStorage.getItem(itemName+'Expiry'))){
-        source = resquestCall();
-        if(source !== sessionStorage.getItem(itemName)){
-            willUpdateCall(source);
+function LiveSession(itemName, resquestCall, willUpdateCall, expiry){
+
+    this.source = null;
+
+    this.run = function(){
+        var currTimestamp = Math.floor(Date.now()/1000);
+        if(this.source == null || (currTimestamp> sessionStorage.getItem(itemName+'Expiry'))){
+            successCall = resquestCall();
+            successCall(
+                function(response){
+                    var stringResponse = JSON.stringify(response);
+                    if(stringResponse !== sessionStorage.getItem(itemName)){
+                        willUpdateCall(response);
+                    }
+                    sessionStorage.setItem(itemName, stringResponse);
+                    sessionStorage.setItem(itemName+'Expiry', currTimestamp+expiry);
+                    this.source = stringResponse;
+                }
+            )
+        }else if(this.source !== sessionStorage.getItem(itemName)){
+            this.source = sessionStorage.getItem(itemName);
+            willUpdateCall(JSON.parse(this.source));
         }
-        sessionStorage.setItem(itemName, source);
-        sessionStorage.setItem(itemName+'Expiry', currTimestamp+interval);
-    }
-
-    return source;
+    }.bind(this);
 }
 
-function resquestCall(){
-    var result = null;
-    ajaxCall('action_name', null, postData).done(
-        function(response){
-            result = response;
+function LiveLocal(itemName, resquestCall, willUpdateCall, expiry){
+
+    this.source = null;
+
+    this.run = function(){
+        var currTimestamp = Math.floor(Date.now()/1000);
+        if(this.source == null || (currTimestamp > localStorage.getItem(itemName+'Expiry'))){
+
+            successCall = resquestCall();
+            successCall(
+                function(response){
+                    var stringResponse = JSON.stringify(response);
+                    if(stringResponse !== localStorage.getItem(itemName)){
+                        willUpdateCall(response);
+                    }
+                    localStorage.setItem(itemName, stringResponse);
+                    localStorage.setItem(itemName+'Expiry', currTimestamp+expiry);
+                    this.source = stringResponse;
+                }.bind(this)
+            )
+        }else if(this.source !== localStorage.getItem(itemName)){
+            this.source = localStorage.getItem(itemName);
+            willUpdateCall(JSON.parse(this.source));
         }
-    )
-    return result;
-}
-
-function willUpdateCall(response){
-    console.log(response);
-}
-
-setInterval(getSession.bind(null, 'test_1', resquestCall, willUpdateCall, 60000), 10000);
-
+    }.bind(this);
