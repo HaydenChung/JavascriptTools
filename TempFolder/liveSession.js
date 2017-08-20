1fdd07960e632075ff0409e5137155ad332f4f51
Hayden <bbconlinenews@yahoo.com>
2017-08-20 12:38:56 +0800

    Change run() logic and add stop method.
    Include the setInterval logic into the class.

--- TempFolder/liveSession.js
+++ TempFolder/liveSession.js
@@ -1,98 +1,156 @@
 
-//just an idea on using sessionStorage.
 /*******************************************
  * 
+ * Just an idea on using sessionStorage/localStorage.
+ * These classes provide a way to fetch and watch data change and invoke an update method when needed.
+ * 
+ * 
  * @param   string      itemName        Item's name for sessionStorage
- * @param   function    resquestCall    Callback to fetch source,should return another function/promise.
+ * @param   function    requestCall     Callback to fetch source,it should return another function which pass response as the first parameter.(eg. jquery's done(), fetch's then()) or a value.
  * @param   function    willUpdateCall  Callback to invoke when source is updated. 
- * @param   integer     expiry          Seconds before the item is expire, where it will invoke the resquestCall.In other word, when will the data refresh.
+ * @param   integer     expiry          Seconds before the item is expire, where it will invoke the requestCall. In other word, when shall the data refresh.
  * @return  Null.
- * @method  function    run             Pass this method as a parameter of setTimeout or setInterval function.
+ * @method  function    run             Start running this live storage, pass-in a integer as interval in milliseconds, retrun the setInterval's timer.
+ * @method  function    stop            Just end the timer, pass true as the first param will delete the local/session item too.
  * 
  *******************************************/
 
-function LiveSession(itemName, resquestCall, willUpdateCall, expiry){
+function LiveSession(itemName, requestCall, willUpdateCall, expiry){
 
-    this.source = null;
+    this._setItem = function(value, now){
+        sessionStorage.setItem(itemName, value);
+        sessionStorage.setItem(itemName+'Expiry', now+expiry);
+        this.source = value;
+    }.bind(this);
 
-    //contructer of this class, where it will invoke the first loop without condition.
-    var currTimestamp = Math.floor(Date.now()/1000);
-    var successCall = resquestCall();
-    successCall(
-        function(response){
-            var stringResponse = JSON.stringify(response);
-            willUpdateCall(response);
-            sessionStorage.setItem(itemName, stringResponse);
-            sessionStorage.setItem(itemName+'Expiry', currTimestamp+expiry);
-            this.source = stringResponse;
-        }.bind(this)
-    )
-
-    this.run = function(){
+    this._loop = function(){
         var currTimestamp = Math.floor(Date.now()/1000);
+        //If the source is empty, or the item is expired,try fetch the data.
         if(this.source == null || (currTimestamp> sessionStorage.getItem(itemName+'Expiry'))){
-            successCall = resquestCall();
-            successCall(
-                function(response){
-                    var stringResponse = JSON.stringify(response);
-                    if(stringResponse !== sessionStorage.getItem(itemName)){
-                        willUpdateCall(response);
+            calledResult = requestCall();
+            if(typeof calledResult === 'function'){
+                calledResult(
+                    function(response){
+                        var stringResponse = JSON.stringify(response);
+                        if(stringResponse !== sessionStorage.getItem(itemName)){
+                            willUpdateCall(response);
+                        }
+                        this._setItem(stringResponse, currTimestamp);
                     }
-                    sessionStorage.setItem(itemName, stringResponse);
-                    sessionStorage.setItem(itemName+'Expiry', currTimestamp+expiry);
-                    this.source = stringResponse;
+                )
+            }else{
+                var stringResponse = JSON.stringify(calledResult);
+                if(stringResponse !== sessionStorage.getItem(itemName)){
+                    willUpdateCall(stringResponse);
                 }
-            )
+                this._setItem(stringResponse, currTimestamp);
+            }
+
         }else if(this.source !== sessionStorage.getItem(itemName)){
             this.source = sessionStorage.getItem(itemName);
             willUpdateCall(JSON.parse(this.source));
         }
     }.bind(this);
-}
 
-function LiveLocal(itemName, resquestCall, willUpdateCall, expiry){
+    this.run = function(interval){
+        return this._timer = setInterval(this._loop, interval);
+    }.bind(this);
 
+    this.stop = function(forceClose){
+        if(forceClose === true){
+            sessionStorage.removeItem(itemName);
+            sessionStorage.removeItem(itemName+'Expiry');
+        }
+        return clearInterval(this._timer);
+    }.bind(this);
+
+    
+    
     this.source = null;
+    this._timer = null;
 
     //contructer of this class, where it will invoke the first loop without condition.
-    var currTimestamp = Math.floor(Date.now()/1000);
-    var successCall = resquestCall();
-    successCall(
-        function(response){
-            var stringResponse = JSON.stringify(response);
-            willUpdateCall(response);
-            localStorage.setItem(itemName, stringResponse);
-            localStorage.setItem(itemName+'Expiry', currTimestamp+expiry);
-            this.source = stringResponse;
-        }.bind(this)
-    )
+    var createdTimestamp = Math.floor(Date.now()/1000);
+    var calledResult = requestCall();
+    if(typeof calledResult === 'function'){
+        calledResult(
+            function(response){
+                willUpdateCall(response);
+                this._setItem(JSON.stringify(calledResult), createdTimestamp);
+            }.bind(this)
+        )
+    }else{
+        willUpdateCall(calledResult);
+        this._setItem(JSON.stringify(calledResult), createdTimestamp);
+    }
+}
 
+//localStorage live amoung browser's windows, it will save alot of fetch request as all windows will watching at the same local item, and only require one request when the item expired.
 
-    this.run = function(){
-        console.log('invoked');
+function LiveLocal(itemName, requestCall, willUpdateCall, expiry){
+
+    this._setItem = function(value, now){
+        localStorage.setItem(itemName, value);
+        localStorage.setItem(itemName+'Expiry', now+expiry);
+        this.source = value;
+    }.bind(this);
+
+    this._loop() = function(){
         var currTimestamp = Math.floor(Date.now()/1000);
-        if(this.source == null || (currTimestamp > localStorage.getItem(itemName+'Expiry'))){
-
-            var successCall = resquestCall();
-            successCall(
-                function(response){
-                    console.log(successCall);
-                    var stringResponse = JSON.stringify(response);
-                    if(stringResponse !== localStorage.getItem(itemName)){
-                        console.log('will update!');
-                        willUpdateCall(response);
+        if(this.source == null || (currTimestamp> localStorage.getItem(itemName+'Expiry'))){
+            calledResult = requestCall();
+            if(typeof calledResult === 'function'){
+                calledResult(
+                    function(response){
+                        var stringResponse = JSON.stringify(response);
+                        if(stringResponse !== localStorage.getItem(itemName)){
+                            willUpdateCall(response);
+                        }
+                        this._setItem(stringResponse, currTimestamp);
                     }
-                    localStorage.setItem(itemName, stringResponse);
-                    localStorage.setItem(itemName+'Expiry', currTimestamp+expiry);
-                    this.source = stringResponse;
-                }.bind(this)
-            )
+                )
+            }else{
+                var stringResponse = JSON.stringify(calledResult);
+                if(stringResponse !== localStorage.getItem(itemName)){
+                    willUpdateCall(stringResponse);
+                }
+                this._setItem(stringResponse, currTimestamp);
+            }
+
         }else if(this.source !== localStorage.getItem(itemName)){
-            console.log('no fetch but update')
             this.source = localStorage.getItem(itemName);
             willUpdateCall(JSON.parse(this.source));
         }
-
     }.bind(this);
-}
 
+    this.run = function(interval){
+        return this._timer = setInterval(this._loop, interval)
+    }.bind(this);
+
+    this.stop = function(forceClose){
+        if(forceClose === true){
+            localStorage.removeItem(itemName);
+            localStorage.removeItem(itemName+'Expiry');
+        }
+        return clearInterval(this._timer);
+    }.bind(this);
+
+
+    this.source = null;
+    this._timer = null;
+
+    //contructer of this class, where it will invoke the first loop without condition.
+    var createdTimestamp = Math.floor(Date.now()/1000);
+    var calledResult = requestCall();
+    if(typeof calledResult === 'function'){
+        calledResult(
+            function(response){
+                willUpdateCall(response);
+                this._setItem(JSON.stringify(calledResult), createdTimestamp);
+            }.bind(this)
+        )
+    }else{
+        willUpdateCall(calledResult);
+        this._setItem(JSON.stringify(calledResult), createdTimestamp);
+    }
+}
